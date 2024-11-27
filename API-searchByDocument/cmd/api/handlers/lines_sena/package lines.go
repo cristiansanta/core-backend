417c203954b75319e2c8792db19139d1e1e53995
MaiLear <mairotavista@gmail.com>
2024-11-27 08:50:33 -0500

    feat: some change in API-searchByDocument

--- API-searchByDocument/cmd/api/handlers/lines_sena/package lines.go
+++ API-searchByDocument/cmd/api/handlers/lines_sena/package lines.go
@@ -1,13 +1,16 @@
 package lines_sena
 
 import (
+	// "SEARCHBYDOCUMENT/internal/domain"
 	"SEARCHBYDOCUMENT/internal/domain"
 	"database/sql"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetLineaAtencion(c *gin.Context, db *sql.DB) {
+	lineasAtencion := []domain.LineaAtencion{}
 	departamento := c.Param("departamento")
 	if departamento == "" {
 		c.JSON(400, gin.H{"error": "El parámetro 'departamento' es requerido"})
@@ -25,19 +28,33 @@ func GetLineaAtencion(c *gin.Context, db *sql.DB) {
               WHERE departamento = $1;`
 
 	// Ejecutar la consulta con el parámetro dinámico
-	row := db.QueryRow(query, departamento)
+	rows,err := db.Query(query, departamento)
 
-	var result domain.LineaAtencion
-	if err := row.Scan(&result.Departamento, &result.Ciudad, &result.Contacto, &result.Direccion, &result.HorarioAtencion, &result.Telefono); err != nil {
-		if err == sql.ErrNoRows {
-			c.JSON(404, gin.H{"error": "No se encontraron resultados"})
-		} else {
-			c.JSON(500, gin.H{"error": "Error al ejecutar la consulta: " + err.Error()})
-		}
+	if err != nil{
+		fmt.Printf("could not do query in GetLineaAtencion:%s",err.Error())
+		c.JSON(500, gin.H{"error": "Server internal error"})
 		return
 	}
+	
+	var result domain.LineaAtencion
+	for rows.Next(){
+		if err := rows.Scan(&result.Departamento, &result.Ciudad, &result.Contacto, &result.Direccion, &result.HorarioAtencion, &result.Telefono); err != nil {
+			if err == sql.ErrNoRows {
+				c.JSON(404, gin.H{"error": "No se encontraron resultados"})
+			} else {
+				c.JSON(500, gin.H{"error": "Error al ejecutar la consulta: " + err.Error()})
+			}
+			return
+		}
 
-	c.JSON(200, gin.H{"data": result})
+		lineasAtencion =  append(lineasAtencion,result)
+
+
+	}
+
+
+
+	c.JSON(200, gin.H{"data": lineasAtencion})
 }
 
 
